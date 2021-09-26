a48d421868a421b984cf4b7b0893cb8490f6fc70
Uma Sivakumar <uma.bhadriraju@gmail.com>
2021-09-25 22:02:38 -0500

    (Deliverable 3)

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -23,3 +23,20 @@ Total_Summary <- Suspension_Coil_Data %>% summarize(Mean=mean(PSI),Median=median
 Lot_Summary <- Suspension_Coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
 
 # DELIVERABLE 3 
+?t.test()
+
+#To determine if PSI across all lots is different from population mean
+t.test(Suspension_Coil_Data$PSI,mu=1500)
+
+# To determine if PSI of each lot is diiferent from population mean 
+
+# Perform t-test on Lot 1
+t.test(subset(Suspension_Coil_Data,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+
+# Perform t-test on Lot 2
+t.test(subset(Suspension_Coil_Data,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+
+# Perform t-test on Lot 3
+t.test(subset(Suspension_Coil_Data,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
+
