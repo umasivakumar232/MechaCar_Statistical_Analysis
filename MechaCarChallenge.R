93886e7dee5fb8fbe7390e5d6ccf7eee994a92ca
Uma Sivakumar <uma.bhadriraju@gmail.com>
2021-09-25 21:03:37 -0500

    (Deliverable 2)

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,7 @@
 library(dplyr)
 
+# DELIVERABLE 1
+
 # Import and read CSV file as a dataframe.
 MechaCar_Data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
@@ -9,3 +11,15 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_Data)) 
 
+# DELIVERABLE 2
+
+# Import and read CSV file as a dataframe.
+Suspension_Coil_Data <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+# Generate Total Summary 
+Total_Summary <- Suspension_Coil_Data %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+# Generate Lot Summary
+Lot_Summary <- Suspension_Coil_Data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+# DELIVERABLE 3 
